6455b71358c428c7098ac6e6e8c02d70d851a808
Sam Noedel <sam.noedel@changepoint.com>
2015-01-09 13:24:31 -0800

    Using morgan for logging

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var store = require('./inMemoryStore');
 
 var app = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
+
 app.set('views', 'src/tmpls');
 app.set('view engine', 'jade');
 
@@ -15,12 +18,7 @@ app.use(function(req, res, next) {
 });
 
 app.post('/v1/data', function (request, response) {
-    console.log('data received:');
-    console.log('==============');
-    console.log(request.body);
-
     store.add(request.body);
-
     response.send();
 });
 
